97a3cf2f758521bcada8b14cd8f262bcb837c69e
Michael McAndrew <mcandrmi@tcd.ie>
2025-09-20 17:59:59 +0100

    Add async/await version

--- promises/promises.js
+++ promises/promises.js
@@ -1,12 +1,33 @@
-fetch("https://jsonplaceholder.typicode.com/users/1")
-  .then((response) => response.json())
-  .then((user) => {
+const getUser = (userId) => {
+  return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+    .then((response) => response.json())
+    .then((user) => {
+      console.log("User name:", user.name);
+      console.log("User email:", user.email);
+    })
+    .catch((error) => {
+      console.log("Error fetching user:", error);
+    });
+};
+
+const user = getUser(1);
+
+// Using async/await
+const getAsyncUser = async (userId) => {
+  try {
+    const userResponse = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${userId}`
+    );
+    const user = await userResponse.json();
     console.log("User name:", user.name);
     console.log("User email:", user.email);
-  })
-  .catch((error) => {
+    return user;
+  } catch (error) {
     console.log("Error fetching user:", error);
-  });
+  }
+};
+
+const userAsync = getAsyncUser(1);
 
 // Promises example activity to get a user and their posts.
 const getUserAndPosts = (userId) => {
