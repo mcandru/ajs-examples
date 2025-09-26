79eeb60403199a223e5dd9e7083202de1c9e738b
Michael McAndrew <mcandrmi@tcd.ie>
2025-09-27 00:36:23 +0100

    Add linting

---
+++ notes-api-part1/eslint.config.js
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: { ...globals.node },
+    },
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+]);
