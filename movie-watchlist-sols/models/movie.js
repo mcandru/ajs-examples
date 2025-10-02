d7c311ef07fcca488b7d0338595b39f35b8a0bc2
Michael McAndrew <mcandrmi@tcd.ie>
2025-10-02 16:30:55 +0100

    Add sols

---
+++ movie-watchlist-sols/models/movie.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const movieSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    watched: {
+      type: Boolean,
+      default: false,
+    },
+    rating: {
+      type: Number,
+      min: 0,
+      max: 10,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+movieSchema.set("toJSON", {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.createdAt;
+    delete returnedObject.updatedAt;
+    delete returnedObject.userId;
+  },
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+export default Movie;
