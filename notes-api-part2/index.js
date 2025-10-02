8fce3d17d40d446cb0e1707420b0a8cdbd50b088
Michael McAndrew <mcandrmi@tcd.ie>
2025-10-02 09:56:33 +0100

    Add startServer with error handling

--- notes-api-part2/index.js
+++ notes-api-part2/index.js
@@ -89,8 +89,16 @@ app.connectDB = async () => {
 };
 
 const PORT = process.env.PORT || 3001;
-app.connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
+const startServer = async () => {
+  try {
+    await app.connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(`Failed to start server: ${error}`);
+    process.exit(1);
+  }
+};
+
+startServer();
