678905f759c060c1595bc019b4bcb6147fd4e771
Michael McAndrew <mcandrmi@tcd.ie>
2025-10-12 21:16:44 +0100

    Merge movie-watchlist-sols

---
+++ movie-watchlist-sols/models/user.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.set("toJSON", {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.createdAt;
+    delete returnedObject.updatedAt;
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
