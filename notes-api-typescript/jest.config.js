bed4deeef718b33c1fe2b9bc0bca1e20c1f2a07f
Michael McAndrew <mcandrmi@tcd.ie>
2025-11-01 16:49:17 +0000

    Remove type checking when running tests for shpeed

--- notes-api-typescript/jest.config.js
+++ notes-api-typescript/jest.config.js
@@ -10,16 +10,19 @@ export default {
 
   // Tell ts-jest to use CommonJS tsconfig
   transform: {
-    "^.+\\.ts$": ["ts-jest", {
-      tsconfig: "tsconfig.jest.json"
-    }]
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.jest.json",
+      },
+    ],
   },
 
   // Map .js imports to their .ts source files
   moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1"
+    "^(\\.{1,2}/.*)\\.js$": "$1",
   },
 
   // Ignore compiled files in dist folder
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"]
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
 };
