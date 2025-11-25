9a1aac99a7f13e7e8f1a52baaab90e667714732c
Michael McAndrew <mcandrmi@tcd.ie>
2025-11-25 09:58:59 +0000

    Add simple input validation on login

--- live-coding/vue-notes/src/views/Login.vue
+++ live-coding/vue-notes/src/views/Login.vue
@@ -2,14 +2,29 @@
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { login } from "@/stores/auth";
+import { loginSchema } from "@/schemas/auth";
+import { z } from "zod";
 
 const router = useRouter();
 
 const email = ref("");
 const password = ref("");
 const isSubmitting = ref(false);
+const inputError = ref("");
 
 const handleSubmit = async () => {
+  try {
+    const result = loginSchema.parse({
+      email: email.value,
+      password: password.value,
+    });
+  } catch (error: unknown) {
+    if (error instanceof z.ZodError) {
+      console.log("Zod error:", error.issues);
+      inputError.value = error.issues[0]?.message || "Invalid login details.";
+    }
+  }
+
   isSubmitting.value = true;
   await login(email.value, password.value);
   isSubmitting.value = false;
@@ -22,9 +37,16 @@ const handleSubmit = async () => {
 
   <div>
     <form @submit.prevent="handleSubmit">
-      <input type="email" placeholder="you@example.com" v-model="email" />
+      <input placeholder="you@example.com" v-model="email" />
       <input type="password" placeholder="Password" v-model="password" />
       <button type="submit" :disabled="isSubmitting">Login</button>
     </form>
+    <div class="input-error">{{ inputError }}</div>
   </div>
 </template>
+
+<style scoped>
+.input-error {
+  color: red;
+}
+</style>
