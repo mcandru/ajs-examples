ee6649a54aff4db3362dc70ed3906547a462080d
Michael McAndrew <mcandrmi@tcd.ie>
2025-12-02 23:03:18 +0000

    Update hints

--- vue-tests/tests/components/Counter.test.ts
+++ vue-tests/tests/components/Counter.test.ts
@@ -2,12 +2,8 @@ import { mount } from "@vue/test-utils";
 import { test, expect } from "vitest";
 import Counter from "@/components/Counter.vue";
 
-// EXERCISE 3: Component Testing Basics
-// Learn how to mount components, find elements, and test user interactions.
-
 test("should render the initial count as 0", () => {
-  // TODO: Mount the Counter component
-  // const wrapper = mount(Counter);
+  const wrapper = mount(Counter);
 
   // TODO: Find the element with data-testid="count-display" and check its text
   // Hint: Use wrapper.find() to find an element
@@ -18,23 +14,23 @@ test("should render the initial count as 0", () => {
 
 test("should increment count when increment button is clicked", async () => {
   // TODO: Mount the Counter component
-  // const wrapper = mount(Counter);
+  const wrapper = mount(Counter);
+
   // TODO: Find the increment button using data-testid="increment-button"
-  // Hint: Use wrapper.find('[data-testid="increment-button"]')
   // TODO: Trigger a click event on the button
-  // Hint: Use await button.trigger('click')
   // TODO: Assert that the count display now shows "Count: 1"
   expect(true).toBe(false);
 });
 
-test("should decrement count when decrement button is clicked", async () => {
+test("should decrement count when decrement button is clicked", async () => 
+  const wrapper = mount(Counter);{
   // TODO: Similar to the increment test, but test the decrement button
   // The count should go from 0 to -1
   expect(true).toBe(false);
 });
 
 test("should reset count to 0 when reset button is clicked", async () => {
-  // TODO: Mount the component
+  const wrapper = mount(Counter);
   // TODO: Click increment a few times to increase the count
   // TODO: Click the reset button
   // TODO: Verify the count is back to 0
--- vue-tests/tests/components/TodoItem.test.ts
+++ vue-tests/tests/components/TodoItem.test.ts
@@ -2,9 +2,6 @@ import { mount } from "@vue/test-utils";
 import { test, expect } from "vitest";
 import TodoItem from "@/components/TodoItem.vue";
 
-// EXERCISE 4: Testing Props and Events
-// Learn how to pass props to components and test emitted events.
-
 test("should render todo text from props", () => {
   const todo = {
     id: 1,
@@ -12,8 +9,7 @@ test("should render todo text from props", () => {
     completed: false,
   };
 
-  // TODO: Mount TodoItem with the todo prop
-  // Hint: const wrapper = mount(TodoItem, { props: { todo } });
+  const wrapper = mount(TodoItem, { props: { todo } });
 
   // TODO: Assert that the component displays the todo text "Learn Vue Testing"
   expect(true).toBe(false);
@@ -26,14 +22,13 @@ test("should apply completed class when todo is completed", () => {
     completed: true,
   };
 
-  // TODO: Mount TodoItem with a completed todo
+  const wrapper = mount(TodoItem, { props: { todo } });
 
   // TODO: Find the li element with data-testid="todo-item"
   // Hint: Use wrapper.find('[data-testid="todo-item"]')
 
   // TODO: Assert that it has the 'completed' class
   // Hint: Use .classes() to get an array of class names
-  // Example: expect(element.classes()).toContain('completed')
   expect(true).toBe(false);
 });
 
@@ -44,18 +39,14 @@ test("should emit toggle event when todo text is clicked", async () => {
     completed: false,
   };
 
-  // TODO: Mount TodoItem with the todo prop
+  const wrapper = mount(TodoItem, { props: { todo } });
 
   // TODO: Find and click the todo text element
   // Hint: data-testid="todo-text-1"
 
   // TODO: Check that the 'toggle' event was emitted
   // Hint: Use wrapper.emitted() to get all emitted events
-  // Example: expect(wrapper.emitted()).toHaveProperty('toggle')
 
-  // TODO: Verify the event payload contains the todo id
-  // Hint: wrapper.emitted('toggle')?.[0] gives you the first emission
-  // The payload should be [1] (the todo id)
   expect(true).toBe(false);
 });
 
@@ -65,6 +56,7 @@ test("should emit delete event when delete button is clicked", async () => {
     text: "Delete me",
     completed: false,
   };
+  const wrapper = mount(TodoItem, { props: { todo } });
 
   // TODO: Mount TodoItem
   // TODO: Find and click the delete button (data-testid="delete-button-2")
