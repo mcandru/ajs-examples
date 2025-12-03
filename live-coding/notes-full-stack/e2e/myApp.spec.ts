89bc2905b8f114b5d4d0e8271f2d4533acbeb332
Michael McAndrew <mcandrmi@tcd.ie>
2025-12-03 10:41:03 +0000

    Add playwright test for local running app

---
+++ live-coding/notes-full-stack/e2e/myApp.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from "@playwright/test";
+const { describe } = test;
+
+describe("Note App", () => {
+  test("front page can be opened", async ({ page }) => {
+    await page.goto("http://localhost:5173/");
+
+    await expect(page.getByText("Notes")).toBeVisible();
+  });
+});
