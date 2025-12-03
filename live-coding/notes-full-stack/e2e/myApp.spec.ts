383339cec0abac6a935d25c93c63e35548cd83b8
Michael McAndrew <mcandrmi@tcd.ie>
2025-12-03 11:10:42 +0000

    Add e2e test to test registration flow

--- live-coding/notes-full-stack/e2e/myApp.spec.ts
+++ live-coding/notes-full-stack/e2e/myApp.spec.ts
@@ -8,4 +8,18 @@ describe("Note App", () => {
     await expect(page.getByText("Notes")).toBeVisible();
     await expect(page.getByText("Welcome back!")).toBeVisible();
   });
+
+  test("user can register with a new account", async ({ page }) => {
+    await page.goto("http://localhost:5173/register");
+
+    const newUserEmail = `testuser_${Date.now()}@example.com`;
+    const newUserPassword = "Password123!";
+
+    await page.getByTestId("form-field-email").fill(newUserEmail);
+    await page.getByTestId("form-field-password").fill(newUserPassword);
+    await page.getByTestId("form-field-confirmPassword").fill(newUserPassword);
+    await page.getByTestId("submit-register").click();
+
+    await expect(page.getByText("Your Notes")).toBeVisible();
+  });
 });
